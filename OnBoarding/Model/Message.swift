e5eeb35fa3c67f0da69ff5ac99b90b56c50fe0c5
mmaalej <Mohamed.Maalej@cognizant.com>
2017-09-26 16:03:32 +0200

    Add Message and MessageResponse models

---
+++ OnBoarding/Model/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  OnBoarding
+//
+//  Created by mmaalej on 26/09/2017.
+//  Copyright © 2017 mmaalej. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    var identifier: String?
+    var associateID: String?
+    var title: String?
+    var body: String?
+    var response: [MessageResponse]?
+}
---
+++ OnBoarding/Model/MessageResponse.swift
@@ -0,0 +1,15 @@
+//
+//  MessageResponse.swift
+//  OnBoarding
+//
+//  Created by mmaalej on 26/09/2017.
+//  Copyright © 2017 mmaalej. All rights reserved.
+//
+
+import Foundation
+
+struct MessageResponse {
+    var identifier: String?
+    var body: String?
+    var MessageIdentifier: String?
+}
